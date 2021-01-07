ddc57f6696b726bdf28ea342669e532b1c29f5bd
Philippe Fanaro <philippefanaro@gmail.com>
2021-01-07 19:53:59 -0300

    pequena melhora de formatação

--- codigo/ui/nav.ts
+++ codigo/ui/nav.ts
@@ -2,7 +2,9 @@ export default class BrNhkNav extends HTMLElement {
   static readonly tag: string = "br-nhk-nav";
 
   private static readonly template: string = `
-    <a href="/"><img src="/midia/logo.png" /></a>
+    <a href="/">
+      <img src="/midia/logo.png" alt="Logo" />
+    </a>
   `;
 
   constructor() {

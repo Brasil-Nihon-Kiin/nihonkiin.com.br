b0607f0c7ed52991a1501b8f9142a67540d8a85b
Philippe Fanaro <philippefanaro@gmail.com>
2021-02-15 16:03:31 -0300

    boilerplate para a formatação do email

--- codigo/dados/jogadores/cadastro_jogador.ts
+++ codigo/dados/jogadores/cadastro_jogador.ts
@@ -4,34 +4,49 @@ export default class BotaoCadastroJogador extends HTMLElement {
   private static readonly moderatorEmail: string = "philippefanaro@gmail.com";
 
   private static readonly template: string = `
-    <button type="submit" id="envio">
-      <a 
-        id="link-envia-email" 
-        href="mailto:${BotaoCadastroJogador.moderatorEmail}">
-        Enviar Email
-      </a>
+    <button type="button" id="envio">
+      <a id="link-envia-email">Enviar Email</a>
     </button>
   `;
 
   private concordaComFaltaDePrivacidade: boolean = false;
+  private emailLink: string = `mailto:${BotaoCadastroJogador.moderatorEmail}?subject=Cadastro de Jogador&body=`;
 
   constructor() {
     super();
   }
 
+  private inicializaEscutaCheckboxDePrivacidade = (): void => {
+    const checkbox = <HTMLInputElement>(
+      this.parentElement!.querySelector("input#ciencia-privacidade")!
+    );
+    checkbox.onclick = (): void => {
+      this.concordaComFaltaDePrivacidade = !this.concordaComFaltaDePrivacidade;
+    };
+  };
+
   connectedCallback() {
     this.innerHTML = BotaoCadastroJogador.template;
 
     this.inicializaEscutaCheckboxDePrivacidade();
+    this.inicializaEscutaDeEnvioDeEmail();
   }
 
-  private inicializaEscutaCheckboxDePrivacidade = (): void => {
-    const checkbox: HTMLInputElement = <HTMLInputElement>(
-      document.querySelector("input#ciencia-privacidade")!
-    );
-
-    checkbox.onclick = (): void => {
-      this.concordaComFaltaDePrivacidade = !this.concordaComFaltaDePrivacidade;
+  private inicializaEscutaDeEnvioDeEmail = (): void => {
+    const botao = <HTMLButtonElement>this.querySelector("button#envio");
+    botao.onclick = (): void => {
+      if (this.concordaComFaltaDePrivacidade) {
+        this.montaEmailLink();
+        console.log(this.emailLink);
+      }
     };
+  };
+
+  private montaEmailLink = (): void => {
+    this.adicionaPrimeiroNome();
+  };
+
+  private adicionaPrimeiroNome = (): void => {
+    this.emailLink += "Philippe";
   };
 }
